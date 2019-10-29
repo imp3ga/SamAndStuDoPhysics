eb0d747511b284b88194598bb75a301d8eac1eb5
imp3ga <30827608+imp3ga@users.noreply.github.com>
2019-10-29 12:32:03 +0000

    clean up

--- include/planets.h
+++ include/planets.h
@@ -3,29 +3,30 @@
 
 #include <ctime>
 #include <chrono>
-typedef std::chrono::high_resolution_clock Clock;
+
+#define CONST_PI 3.14159265358979
 
 class planet{
     public:
-        planet(double r);
-        void setRadius(double r);
-        void setMass(double m);
-        void setPosition(Eigen::Vector2d s);
-        void setVelocity(Eigen::Vector2d v);
+        planet(const double r);
+        void setRadius(const double r);
+        void setMass(const double m);
+        void setPosition(const Eigen::Vector2d s);
+        void setVelocity(const Eigen::Vector2d v);
 
         double getRadius(){return dRadius;};
         double getMass(){return dMass;};
         Eigen::Vector2d getPosition(){return position;};
         Eigen::Vector2d getVelocity(){return velocity;};
-        
 
     private:
         double dRadius, dMass;
         Eigen::Vector2d position, velocity;
 };
 
+typedef std::chrono::high_resolution_clock Clock;
+
 const double _dHalfWindowWidth = 640.0, _dHalfWindowHeight = 480.0, _dMassDensity = 1000.0;
-// std::chrono::time_point _initTime;
 std::chrono::system_clock::time_point _initTime;
-std::vector<planet> solarSystem;
+std::vector<planet> _solarSystem;
 Eigen::Vector2d _initXY;
--- src/planets.cpp
+++ src/planets.cpp
@@ -1,48 +1,44 @@
 #include <GL/freeglut.h>
-#include <cmath>
 #include <iostream>
 
 #include "include/planets.h"
 
-planet::planet(double r)
+planet::planet(const double r)
 {
     setRadius(r);
     setMass(r * r * _dMassDensity);
 }
 
-void planet::setRadius(double r)
+void planet::setRadius(const double r)
 {
     dRadius = r;
 }
 
-void planet::setMass(double m)
+void planet::setMass(const double m)
 {
     dMass = m;
 }
 
-void planet::setPosition(Eigen::Vector2d s)
+void planet::setPosition(const Eigen::Vector2d s)
 {
     position = s;
 }
 
-void planet::setVelocity(Eigen::Vector2d v)
+void planet::setVelocity(const Eigen::Vector2d v)
 {
     velocity = v;
 }
 
 void display(){
-    glClear(GL_COLOR_BUFFER_BIT); // blit blank display
+    glClear(GL_COLOR_BUFFER_BIT);
 
-    
-    const double pi = 3.141593;
-
-    for(int i = 0; i < solarSystem.size(); ++i)
+    for(int i = 0; i < _solarSystem.size(); ++i)
     {
         glBegin(GL_POLYGON);
-        planet &planet = solarSystem[i];
+        planet &planet = _solarSystem[i];
         Eigen::Vector2d pos = planet.getPosition();
         double dRad = planet.getRadius();
-        for(float arc = 0; arc < 2*pi; arc+=0.5){
+        for(float arc = 0; arc < 2*CONST_PI; arc+=0.5){
 
             glVertex2f(dRad*(cos(arc)) + pos[0], dRad*(sin(arc)) + pos[1]);
         }
@@ -55,29 +51,28 @@ void display(){
 
 void physicsLoop(int val){
     display();
-
     // Calculate forces on each object and check for collisions
-    const int nObjects = solarSystem.size();
+    const int nObjects = _solarSystem.size();
     Eigen::Vector2d aForces[nObjects - 1];
     std::vector<planet> newPlanets;
     for (int i = 0; i < nObjects; ++i)
     {
-        planet &p0 = solarSystem[i];
+        planet &p0 = _solarSystem[i];
         Eigen::Vector2d p0Pos = p0.getPosition();
         double p0Mass = p0.getMass();
         if(p0Mass < 0)
         {
             continue;
         }
-        Eigen::Vector2d force(0., 0.);
 
+        Eigen::Vector2d force(0., 0.);
         for (int j = 0; j < nObjects; ++j)
         {
             if(i == j)
             {
                 continue;
             }
-            planet &p1 = solarSystem[j];
+            planet &p1 = _solarSystem[j];
             Eigen::Vector2d p1Pos = p1.getPosition();
             double p1Mass = p1.getMass();
             if(p1Mass < 0.)
@@ -86,68 +81,53 @@ void physicsLoop(int val){
             }
             // Check for collisions
             double dist = (p1Pos - p0Pos).norm();
-            // std::cout << dist << "\n";
             if (dist < p0.getRadius() + p1.getRadius())
             {
                 double newMass = p0Mass + p1Mass;
-                double newRadius = sqrt(newMass / _dMassDensity);
-                // std::cout << "prev masses: " << p0Mass << ", " << p1Mass << ",   new mass: " << newMass << "\n";
-                // std::cout << "prev radii: " << p0.getRadius() << ", " << p1.getRadius() << ",   new radius: " << newRadius << "\n";
-                // std::cout << "dist was: " << dist << "\n\n";
-                // std::cout << "i, j: " << i << ", " << j << "\n\n";
+                double newRadius = sqrt(newMass / _dMassDensity);       // Preferably remove this sqrt somehow
                 planet pNew(newRadius);
-
-                pNew.setPosition(p0Mass > p1Mass ? p0.getPosition() : p1.getPosition());
+                pNew.setPosition(p0Mass > p1Mass ? p0Pos : p1Pos);
                 Eigen::Vector2d p0Vel = p0.getVelocity();
                 Eigen::Vector2d p1Vel = p1.getVelocity();
                 Eigen::Vector2d newVel = (p0Mass * p0Vel + p1Mass * p1Vel) / newMass;
                 pNew.setVelocity(newVel);
                 newPlanets.push_back(pNew);
-                solarSystem[i].setMass(-1.0);
-                solarSystem[j].setMass(-1.0);
+                _solarSystem[i].setMass(-1.0); _solarSystem[j].setMass(-1.0);
                 continue;
             }
             double dForce = p0Mass * p1Mass / (p1Pos - p0Pos).squaredNorm();
-
             force += dForce * (p1Pos - p0Pos);
         }
         aForces[i] = force;
-        // std::cout << "Total force on object " << i << " is " << force.transpose() << "\n";
     }
-
-
-        // Remove negative mass planets, add new ones
+        // Remove negative mass planets (ie collided objects), add new ones
         for (int i = 0; i < nObjects; ++i)
         {
-            planet &planet = solarSystem[i];
+            planet &planet = _solarSystem[i];
             double m = planet.getMass();
             if(m < 0.0)
             {
-                solarSystem.erase(solarSystem.begin() + i);
+                _solarSystem.erase(_solarSystem.begin() + i);
                 ++i;
             }
         }
-        solarSystem.insert( solarSystem.end(), newPlanets.begin(), newPlanets.end() );
+        _solarSystem.insert(_solarSystem.end(), newPlanets.begin(), newPlanets.end());
 
-    // std::cout << "FINISHED check \n";
-
+    // Updates
     for (int i = 0; i < nObjects; ++i)
     {
-        planet &planet = solarSystem[i];
+        planet &planet = _solarSystem[i];
         // Update velocity
         double mass = planet.getMass();
         Eigen::Vector2d acc = aForces[i] / mass;
-        Eigen::Vector2d newVel = planet.getVelocity() + (0.001 * acc);
+        Eigen::Vector2d newVel = planet.getVelocity() + (0.001 * acc);      // Physics update is 1ms
         planet.setVelocity(newVel);
         // Update position
-        Eigen::Vector2d newPos = planet.getPosition() + (0.001 * newVel);
+        Eigen::Vector2d newPos = planet.getPosition() + (0.001 * newVel);   // Physics update is 1ms
         planet.setPosition(newPos);
     }
-
-    glutTimerFunc(1, physicsLoop, 0);
+    glutTimerFunc(1, physicsLoop, 0);                                       // 1ms
 }
-
-
 
 void mouseHandler(int button, int state, int x, int y) 
 {
@@ -171,17 +151,19 @@ void mouseHandler(int button, int state, int x, int y)
         planet newPlanet(500.0 * t * t);
         newPlanet.setPosition(pos);
         newPlanet.setVelocity(v);
-        solarSystem.push_back(newPlanet);
+        _solarSystem.push_back(newPlanet);
     }
   }
 }
 
 int main(int argc, char **argv){
+    // Add the initial large planet
     planet initPlanet(50);
-    initPlanet.setPosition(Eigen::Vector2d(0, 0));
+    initPlanet.setPosition(Eigen::Vector2d(0., 0.));
     initPlanet.setVelocity(Eigen::Vector2d(0.,0.));
-    solarSystem.push_back(initPlanet);
+    _solarSystem.push_back(initPlanet);
 
+    // openGL initialization
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE);
     glutInitWindowSize(_dHalfWindowWidth * 2.0, _dHalfWindowHeight * 2.0);
@@ -195,7 +177,6 @@ int main(int argc, char **argv){
 
     glutMainLoop();
    
-
     return 0;
 }
 
