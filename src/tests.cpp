9d9b791322c5a5f1f8842be31f4662668188fb92
Stuart Aitken <s.aitken88@gmail.com>
2019-11-06 13:28:06 +0800

    Altered Velocity unit test so it works again

--- src/tests.cpp
+++ src/tests.cpp
@@ -38,24 +38,23 @@ void UnitTest::RunRests(){
 //Test head on collision of two equal objects. Expect basic reversal of velocities.
 void UnitTest::TestVelocityAfterCollision(){
      
-    // solarSystem _solarSys = solarSystem(10);
+    solarSystem _solarSys = solarSystem(10);
 
-    // planet p0 = planet(1, 1,false);
-    // planet p1 = planet(1, 1, false);
+    planet p0 = planet(1, 1,false);
+    planet p1 = planet(1, 1, false);
 
-    // p0.setPosition(Eigen::Vector2d(-1,0));
-    // p0.setPosition(Eigen::Vector2d(1,0));
+    p0.setPosition(Eigen::Vector2d(-1,0));
+    p0.setPosition(Eigen::Vector2d(1,0));
 
-    // p0.setVelocity(Eigen::Vector2d(5,0));
-    // p1.setVelocity(Eigen::Vector2d(-5,0));
+    p0.setVelocity(Eigen::Vector2d(5,0));
+    p1.setVelocity(Eigen::Vector2d(-5,0));
 
-    // Eigen::Vector2d p0NewVel, p1NewVel;
-    // _solarSys.twoBodyCollision(p0,p1, p0NewVel, p1NewVel);
-    // UnitTest CollisionVelocityTest_P0 = UnitTest("Collision Velocity Test P0");
-    // CollisionVelocityTest_P0.AssertEqual(Eigen::Vector2d(-5,0), p0NewVel);
+    _solarSys.twoBodyCollision(p0,p1,false);
+    UnitTest CollisionVelocityTest_P0 = UnitTest("Collision Velocity Test P0");
+    CollisionVelocityTest_P0.AssertEqual(Eigen::Vector2d(-5,0), p0.getVelocity());
 
-    // UnitTest CollisionVelocityTest_P1 = UnitTest("Collision Velocity Test P1");
-    // CollisionVelocityTest_P1.AssertEqual(Eigen::Vector2d(5,0), p1NewVel);
+    UnitTest CollisionVelocityTest_P1 = UnitTest("Collision Velocity Test P1");
+    CollisionVelocityTest_P1.AssertEqual(Eigen::Vector2d(5,0), p1.getVelocity());
 
     
 }
